17035fdbdffd431fbfe873f6fa929cb84e5d7b17
jgindis <jgindis20@stuy.edu>
2020-10-27 18:25:38 -0400

    Added a constructor

--- BankAccount.java
+++ BankAccount.java
@@ -5,3 +5,9 @@
 //       balance - a double
 //       accountID - int  (the account number is just an integer)
 //       password - String
+
+public BankAccount(int accountID, String password) {
+  int ID = accountID;
+  String pw = password;
+  double balance = 0;
+}
