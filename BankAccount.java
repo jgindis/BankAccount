845f95b314bacdc58e39cbe40a66f50c80fe4837
jgindis <jgindis20@stuy.edu>
2020-10-27 18:11:38 -0400

    Initial Commit

---
+++ BankAccount.java
@@ -0,0 +1,7 @@
+// class name:
+//         BankAccount
+//
+// Instance variables required (all private):
+//       balance - a double
+//       accountID - int  (the account number is just an integer)
+//       password - String
